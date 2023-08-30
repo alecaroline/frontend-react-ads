eec7c7cfcfd187522321f8e0aa4a878ba8d4bf2c
Alessandra Caroline <alessandracarolineavelino@gmail.com>
2023-08-29 22:31:46 -0300

    Criação da Header

---
+++ src/app/quem-somos/page.js
@@ -0,0 +1,9 @@
+//Componente no React --> função que devolve um trecho de HTML
+
+const QuemSomos = () => {
+  return (
+    <h1>Bem vindo ao "Quem somos!"</h1>
+  )
+}
+
+export default QuemSomos
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,20 @@
+// import React from 'react'
+
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header id="#header">
+        <h1>Logo</h1>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Quem somos</li>
+                <li>Contato</li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
